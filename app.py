42e50f1fce8224151b77456bce4664627c3cf984
FranPandol <pandol.francisco@gmail.com>
2021-07-17 13:17:41 -0300

    Add method to send message to SQS queue with payment_id from mercadopago notification

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 import json
 import logging
 import sys
+import boto3
 
 from flask import Flask, request, abort, Response
+from botocore.exceptions import ClientError
 
 
 # Initialize Flask app
@@ -12,6 +14,38 @@ app = Flask(__name__)
 app.config.from_object('config.ProductionConfig')
 
 logger = logging.getLogger(__name__)
+
+sqs = boto3.client(
+    'sqs',
+    aws_access_key_id=app.config['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=app.config['AWS_SECRET_ACCESS_KEY'],
+    region_name=app.config['AWS_DEFAULT_REGION']
+)
+
+queue_url = app.config['AWS_SQS_QUEUE_URL']
+
+
+def send_message(payment_id):
+    # Send message to SQS queue
+
+    try:
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            DelaySeconds=10,
+            MessageAttributes={
+                'payment_id': {
+                    'DataType': 'String',
+                    'StringValue': payment_id
+                },
+            },
+            MessageBody=(
+                payment_id
+            ),
+        )
+        return response
+    except ClientError as error:
+        logger.exception("Send message failed payment_id: {}".format(payment_id))
+        return None
 
 
 @app.route('/notifications', methods=['POST'])
@@ -23,4 +57,5 @@ def notifications():
         return Response(status=200)
 
     payment_id = request.args.get('id')
+    send_message(payment_id)
     return Response(status=200)
--- config.py
+++ config.py
@@ -9,3 +9,7 @@ class BaseConfig(object):
 class ProductionConfig(BaseConfig):
     DEVELOPMENT = False
     DEBUG = False
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
+    AWS_SQS_QUEUE_URL = os.environ.get('AWS_SQS_QUEUE_URL')
